e003a08c2e67119b21a04e0b48cf8f12112f47c0
Zhiqiang Zhao <learningambassador@163.com>
2026-01-03 21:14:07 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,8 @@ html_theme_options = {
 }
 
 html_theme_options = {
-    "extra_navbar": '<a href="https://test-website-en.readthedocs.io/en/latest/">返回文档中心</a>',
+    # "extra_navbar": '<a href="https://test-website-en.readthedocs.io/en/latest/">返回文档中心</a>',
+    "extra_footer": "<div><a href="https://test-website-en.readthedocs.io/en/latest/">返回文档中心</a></div>",
     "logo": {
       "image_light": "_static/kernelgen-logo.svg",
       "image_dark": "_static/kernelgen-logo.svg",
