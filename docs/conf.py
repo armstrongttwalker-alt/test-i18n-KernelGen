27d7af6ef4858eb6dc8cdaa67538ad75a3b65c01
Zhiqiang Zhao <learningambassador@163.com>
2026-01-08 15:46:05 +0800

    fix pdf

--- docs/conf.py
+++ docs/conf.py
@@ -138,23 +138,28 @@ myst_enable_extensions = [
 ]
 htmlhelp_basename = "KernelGendoc"
 
-# conf.py
-
-# 语言配置（基于你的现有代码）
-language = "en" if docset == "en" else "zh_CN"
 
 # 基础LaTeX配置
-latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '10pt',
-    'figure_align': 'htbp',
-    'sphinxsetup': 'TitleColor={rgb}{0,0,0}, InnerLinkColor={rgb}{0,0,0}, OuterLinkColor={rgb}{0,0,0}',
-}
+latex_engine = 'xelatex'  # 必须使用xelatex支持中文
 
-# 根据docset设置不同的LaTeX配置
+# 根据语言配置latex_documents
 if docset == "en":
-    # 英文LaTeX配置
-    latex_elements.update({
+    # 英文配置
+    latex_documents = [
+        (
+            "index",
+            "KernelGen.tex",  # 保持相同文件名，但放在不同目录
+            "KernelGen Documentation",
+            "KernelGen Team",
+            "manual",
+        ),
+    ]
+    
+    # 英文特定的LaTeX元素
+    latex_elements = {
+        'papersize': 'a4paper',
+        'pointsize': '10pt',
+        'figure_align': 'htbp',
         'preamble': r'''
 \usepackage{fontspec}
 \setmainfont{Times New Roman}
@@ -164,29 +169,38 @@ if docset == "en":
 ''',
         'babel': r'\usepackage[english]{babel}',
         'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
-    })
+        'classoptions': ',oneside',
+        'printindex': r'''
+\clearpage
+\phantomsection
+\addcontentsline{toc}{chapter}{\indexname}
+\printindex
+''',
+    }
     
-    # 英文文档元数据
+else:
+    # 中文配置
     latex_documents = [
-        ('index', 'document.tex', 'Your English Title', 'Author Name', 'manual'),
+        (
+            "index",
+            "KernelGen.tex",  # 保持相同文件名，但放在不同目录
+            "KernelGen 文档",
+            "KernelGen 团队",
+            "manual",
+        ),
     ]
     
-    # 其他英文特定设置
-    latex_logo = '_static/logo_en.png' if os.path.exists('_static/logo_en.png') else None
-    
-else:  # zh_CN 中文配置
-    # 中文LaTeX配置
-    latex_use_xindy = False
+    # 中文特定的LaTeX元素
     latex_elements = {
+        'papersize': 'a4paper',
+        'pointsize': '10pt',
+        'figure_align': 'htbp',
         'preamble': r'''
 \usepackage[UTF8]{ctex}
 \usepackage{xeCJK}
-\setmainfont{Times New Roman}
-\setsansfont{Arial}
-\setmonofont{Courier New}
 \setCJKmainfont{SimSun}
 \setCJKsansfont{SimHei}
-\setCJKmonofont{FangSong}
+\setCJKmonofont{SimSun}
 \XeTeXlinebreaklocale "zh"
 \XeTeXlinebreakskip = 0pt plus 1pt
 \usepackage{indentfirst}
@@ -195,31 +209,23 @@ else:  # zh_CN 中文配置
         'babel': '',  # 禁用babel，使用ctex
         'classoptions': ',oneside',
         'fncychap': r'\usepackage[Sonny]{fncychap}',
-    })
-    
-    # 重命名LaTeX中的章节名称
-    latex_elements['preamble'] += r'''
-\addto\captionsenglish{\renewcommand{\contentsname}{目录}}
-\addto\captionsenglish{\renewcommand{\figurename}{图}}
-\addto\captionsenglish{\renewcommand{\tablename}{表}}
-\addto\captionsenglish{\renewcommand{\listfigurename}{插图目录}}
-\addto\captionsenglish{\renewcommand{\listtablename}{表格目录}}
-\addto\captionsenglish{\renewcommand{\indexname}{索引}}
-'''
-    
-    # 中文文档元数据
-    latex_documents = [
-        ('index', 'document.tex', '中文文档标题', '作者', 'manual'),
-    ]
-    
-    # 其他中文特定设置
-    latex_logo = '_static/logo_zh.png' if os.path.exists('_static/logo_zh.png') else None
+        'printindex': r'''
+\clearpage
+\phantomsection
+\addcontentsline{toc}{chapter}{索引}
+\printindex
+''',
+    }
 
-# 设置LaTeX引擎（中文推荐使用xelatex）
-if docset != "en":
-    latex_engine = 'xelatex'  # 中文使用xelatex
-else:
-    latex_engine = 'xelatex'  # 英文可以使用pdflatex或xelatex
+man_pages = [
+    (
+        "index",
+        "kernelgen",
+        "KernelGen Documentation",
+        ["KernelGen Team"],
+        1,
+    )
+]
 
 language = "en" if docset == "en" else "zh_CN"
 
